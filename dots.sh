296828a4bc99b46735628884cdd9f70b82ece763
The-BB <tun.chen.bo@gmail.com>
2020-02-14 14:47:16 +0300

    dots.sh: refresh

--- dots.sh
+++ dots.sh
@@ -8,10 +8,10 @@ seconds=120
 timer=$(($(date +%s) + seconds))
 
 while [ "$timer" -ge "$(date +%s)" ]; do
-  if [ -f "/opt/etc/init.d/S51dropbear" ]; then
+  if [ -x "/opt/etc/init.d/S51dropbear" ]; then
 	break
   fi
-	echo " in progress..." && sleep 1
+	echo "... in progress..." && sleep 1
 done
 
 rm "$0"
