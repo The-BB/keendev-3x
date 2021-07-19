108bd9bb3814eed683024b1663a9dd77a2841cc1
The-BB <tun.chen.bo@gmail.com>
2021-07-19 07:21:56 +0300

    EN_make-installer-aarch64.sh: add

---
+++ EN_make-installer-aarch64.sh
@@ -0,0 +1,68 @@
+#!/bin/sh
+
+# Working dir should stay in feeds/keendev3x
+SCRIPT_DIR=$(dirname $0)
+ROOT_DIR=$SCRIPT_DIR/installer_root
+BUILD_DIR=$SCRIPT_DIR/../../build_dir/target-aarch64_cortex-a53_glibc-2.27*
+INSTALLER=$SCRIPT_DIR/EN_aarch64-installer.tar.gz
+
+[ -d $ROOT_DIR ] && rm -fr $ROOT_DIR
+mkdir $ROOT_DIR
+
+# Adding toolchain libraries
+cp -r $BUILD_DIR/toolchain/ipkg-aarch64-3.10/libc/opt $ROOT_DIR
+cp -r $BUILD_DIR/toolchain/ipkg-aarch64-3.10/libgcc/opt $ROOT_DIR
+cp -r $BUILD_DIR/toolchain/ipkg-aarch64-3.10/libpthread/opt $ROOT_DIR
+cp -r $BUILD_DIR/toolchain/ipkg-aarch64-3.10/librt/opt $ROOT_DIR
+cp -r $BUILD_DIR/toolchain/ipkg-aarch64-3.10/libssp/opt $ROOT_DIR
+
+# Adding locales
+mkdir -p $ROOT_DIR/opt/usr/lib/locale
+cp -r $SCRIPT_DIR/locale-archive.2.27-aarch64 $ROOT_DIR/opt/usr/lib/locale/locale-archive
+
+# Adding busybox
+cp -r $BUILD_DIR/busybox-default/busybox-*/ipkg-install/opt $ROOT_DIR
+
+# Copying (replace) busybox strip version
+cp -fr $BUILD_DIR/busybox-default/busybox-*/ipkg-aarch64-3.10/busybox/opt $ROOT_DIR
+
+# Removing some busybox symlinks
+[ -L "$ROOT_DIR/opt/bin/egrep" ] && rm -f "$ROOT_DIR/opt/bin/egrep"
+[ -L "$ROOT_DIR/opt/bin/fgrep" ] && rm -f "$ROOT_DIR/opt/bin/fgrep"
+[ -L "$ROOT_DIR/opt/bin/grep" ] && rm -f "$ROOT_DIR/opt/bin/grep"
+
+# Adding libpcre&grep
+cp -fr $BUILD_DIR/grep-*/ipkg-aarch64-3.10/grep/opt $ROOT_DIR
+cp -r $BUILD_DIR/pcre-*/ipkg-aarch64-3.10/libpcre/opt $ROOT_DIR
+
+# Adding libndm&ndmq
+cp -r $BUILD_DIR/ndmq-*/ipkg-aarch64-3.10_kn/ndmq/opt $ROOT_DIR
+cp -r $BUILD_DIR/libndm-*/ipkg-aarch64-3.10_kn/libndm/opt $ROOT_DIR
+
+# Adding dummie SSH keys to avoid dropbear post-install timeout
+mkdir -p $ROOT_DIR/opt/etc/dropbear
+touch $ROOT_DIR/opt/etc/dropbear/dropbear_ecdsa_host_key
+touch $ROOT_DIR/opt/etc/dropbear/dropbear_ed25519_host_key
+touch $ROOT_DIR/opt/etc/dropbear/dropbear_rsa_host_key
+
+# Adding opkg&opkg.conf
+cp -r $BUILD_DIR/linux-aarch64-3.10/opkg-*/ipkg-aarch64-3.10/opkg/opt $ROOT_DIR
+cp -r $SCRIPT_DIR/opkg-aarch64.conf $ROOT_DIR/opt/etc/opkg.conf
+chmod 644 $ROOT_DIR/opt/etc/opkg.conf
+
+# Removing starting scripts
+rm -f $ROOT_DIR/opt/etc/init.d/S*
+
+# Adding install script
+mkdir -p $ROOT_DIR/opt/etc/
+cp $SCRIPT_DIR/initrc $ROOT_DIR/opt/etc/
+chmod +x $ROOT_DIR/opt/etc/initrc
+cp $SCRIPT_DIR/EN_doinstall $ROOT_DIR/opt/bin/doinstall
+chmod +x $ROOT_DIR/opt/bin/doinstall
+
+# Packing installer
+[ -f $INSTALLER ] && rm $INSTALLER
+tar -czf $INSTALLER -C $ROOT_DIR/opt --owner=root --group=root bin etc lib sbin usr
+
+# Removing temp folder
+rm -fr $ROOT_DIR
