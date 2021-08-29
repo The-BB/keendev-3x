9f8f931a48af35f188a067a2ea7249917e648be0
The-BB <tun.chen.bo@gmail.com>
2021-08-29 12:59:17 +0300

    EN_make-installer-mips.sh: update

--- EN_make-installer-mips.sh
+++ EN_make-installer-mips.sh
@@ -50,11 +50,11 @@ chmod 644 $ROOT_DIR/opt/etc/opkg.conf
 rm -f $ROOT_DIR/opt/etc/init.d/S*
 
 # Adding install script
-mkdir -p $ROOT_DIR/opt/etc/
-cp $SCRIPT_DIR/initrc $ROOT_DIR/opt/etc/
-chmod +x $ROOT_DIR/opt/etc/initrc
-cp $SCRIPT_DIR/EN_doinstall $ROOT_DIR/opt/bin/doinstall
-chmod +x $ROOT_DIR/opt/bin/doinstall
+mkdir -p $ROOT_DIR/opt/etc/init.d
+cp $SCRIPT_DIR/doinstall $ROOT_DIR/opt/etc/init.d/
+chmod +x $ROOT_DIR/opt/etc/init.d/doinstall
+cp $SCRIPT_DIR/EN_install $ROOT_DIR/opt/bin/install
+chmod +x $ROOT_DIR/opt/bin/install
 
 # Packing installer
 [ -f $INSTALLER ] && rm $INSTALLER
