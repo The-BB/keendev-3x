a1f88f50563dfb72e5a6f89e07a5fe00b79b0b9c
The-BB <tun.chen.bo@gmail.com>
2022-12-15 18:21:28 +0300

    EN_make-installer-mipsel.sh: fix grep

--- EN_make-installer-mipsel.sh
+++ EN_make-installer-mipsel.sh
@@ -34,6 +34,7 @@ cp -fr $BUILD_DIR/busybox-default/busybox-*/ipkg-mipsel-3.4/busybox/opt $ROOT_DI
 # Adding libpcre&grep
 cp -fr $BUILD_DIR/grep-*/ipkg-mipsel-3.4/grep/opt $ROOT_DIR
 cp -r $BUILD_DIR/pcre-*/ipkg-mipsel-3.4/libpcre/opt $ROOT_DIR
+ln -sf ../libexec/grep-gnu $ROOT_DIR/opt/bin/grep
 
 # Adding dummie SSH keys to avoid dropbear post-install timeout
 mkdir -p $ROOT_DIR/opt/etc/dropbear
@@ -58,7 +59,7 @@ chmod +x $ROOT_DIR/opt/bin/install
 
 # Packing installer
 [ -f $INSTALLER ] && rm $INSTALLER
-tar -czf $INSTALLER -C $ROOT_DIR/opt --owner=root --group=root bin etc lib sbin usr
+tar -czf $INSTALLER -C $ROOT_DIR/opt --owner=root --group=root bin etc lib libexec sbin usr
 
 # Removing temp folder
 rm -fr $ROOT_DIR
